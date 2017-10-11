03f6cd48b5ad41f12d6a3dcb203667cff6cf0473
Mark Hahnenberg <mark@nylas.com>
2017-10-11 13:29:03 -0700

    Bump package version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='mypytools',
-      version='0.1.3',
+      version='0.1.4',
       description='A bundle of tools to make using mypy easier',
       url='https://github.com/nylas/mypy-tools',
       license='MIT',
